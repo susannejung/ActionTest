39a32cd1e81ccf6f269ded03c8c8f426d1c47a4a
susannejung <95688233+susannejung@users.noreply.github.com>
2022-01-25 15:42:37 +0100

    Update Main.java
    Commit2

--- src/PakkeNy/Main.java
+++ src/PakkeNy/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Test action");
+	     System.out.print("Test action2");
     }
 }
